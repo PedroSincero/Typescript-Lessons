e82536b175b1bd36441825ec697956442199d5a3
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-04-07 18:39:37 -0300

    2.2 - exemplo com tipo string

--- 2-Foundations/materials/index.js
+++ 2-Foundations/materials/index.js
@@ -1,3 +1,4 @@
+// 1 - number
 var x = 10;
 var y = 18.4156456343;
 x = 80;
@@ -5,3 +6,11 @@ console.log(x);
 console.log(typeof x);
 console.log(y);
 console.log(y.toPrecision(3));
+// 2 - string
+var firstName = "Pedro";
+console.log(firstName.toLowerCase());
+var fullName;
+var lastName = "Henrique";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
--- 2-Foundations/materials/index.ts
+++ 2-Foundations/materials/index.ts
@@ -1,3 +1,4 @@
+// 1 - number
 let x: number = 10;
 
 
@@ -10,3 +11,19 @@ console.log(typeof x);
 console.log(y);
 
 console.log(y.toPrecision(3));
+
+// 2 - string
+
+const firstName: string = "Pedro";
+
+console.log(firstName.toLowerCase());
+
+let fullName: string;
+
+const lastName: string = "Henrique"
+
+fullName = firstName + " " + lastName;
+
+console.log(fullName);
+console.log(typeof fullName);
+
