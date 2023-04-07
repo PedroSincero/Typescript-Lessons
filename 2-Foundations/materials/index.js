562469bd86ecd80dc66ee513201f6ade7b423213
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-04-07 18:32:31 -0300

    2.1 - exercicio com tipo numerico

---
+++ 2-Foundations/materials/index.js
@@ -0,0 +1,7 @@
+var x = 10;
+var y = 18.4156456343;
+x = 80;
+console.log(x);
+console.log(typeof x);
+console.log(y);
+console.log(y.toPrecision(3));
---
+++ 2-Foundations/materials/index.ts
@@ -0,0 +1,12 @@
+let x: number = 10;
+
+
+const y:number = 18.4156456343
+
+x = 80;
+console.log(x);
+
+console.log(typeof x);
+console.log(y);
+
+console.log(y.toPrecision(3));
