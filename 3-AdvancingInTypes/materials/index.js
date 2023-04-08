75fe2e25986f700c8d8aad361765ae9cf958434e
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-04-08 18:54:29 -0300

    3.1 - exemplo de arrays

---
+++ 3-AdvancingInTypes/materials/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// 1 - arrays 
+let numbers = [1, 2, 3];
+numbers.push(5);
+console.log(numbers[2]);
+numbers = [];
+// numbers = "oi";
+let names = ['oibbo', 'oi', 'oo', 'helow'];
+names.push('dsd');
---
+++ 3-AdvancingInTypes/materials/index.ts
@@ -0,0 +1,15 @@
+// 1 - arrays 
+
+let numbers: number[] = [1,2,3];
+
+numbers.push(5);
+
+console.log(numbers[2]);
+
+numbers = [];
+
+// numbers = "oi";
+
+let names: string[] = ['oibbo', 'oi', 'oo', 'helow'];
+
+names.push('dsd');
