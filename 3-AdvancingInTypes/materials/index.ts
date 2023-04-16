73faa0674ea33db20a421fffd1a2bbd6d925308f
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-04-16 15:26:06 -0300

    3.2 - exemplo sintaxe arrays

--- 3-AdvancingInTypes/materials/index.ts
+++ 3-AdvancingInTypes/materials/index.ts
@@ -13,3 +13,15 @@ numbers = [];
 let names: string[] = ['oibbo', 'oi', 'oo', 'helow'];
 
 names.push('dsd');
+
+// 2 - outra sintaxe array
+
+const num: Array<number> = [100, 200];
+
+num.push(300);
+
+console.log(num);
+
+// num.push('131')
+
+console.log(num[0]);
