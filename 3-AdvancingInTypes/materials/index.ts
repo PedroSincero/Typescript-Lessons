6a6a43e8278096d52a858f86a80f2523e4901ecf
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-04-26 22:17:21 -0300

    3.9 - exemplo union type

--- 3-AdvancingInTypes/materials/index.ts
+++ 3-AdvancingInTypes/materials/index.ts
@@ -94,3 +94,14 @@ function advancedGreeting(firstName: string, lastName?: string) {
 
 advancedGreeting('pedro');
 advancedGreeting('pedro', 'henrique');
+
+// 10 - union type
+
+function showBalance(balance: string | number) {
+  console.log(`O saldo da conta é: ${balance}`);
+};
+
+showBalance(100);
+showBalance("500");
+
+const arr2: Array<number | string | boolean> = [1, 'teste', true];
