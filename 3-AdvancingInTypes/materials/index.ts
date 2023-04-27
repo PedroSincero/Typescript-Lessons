469f3838c9735789aa6605622d4a7d67275d7a8d
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-04-26 22:27:05 -0300

    3.10 - exemplo validando union type

--- 3-AdvancingInTypes/materials/index.ts
+++ 3-AdvancingInTypes/materials/index.ts
@@ -105,3 +105,14 @@ showBalance(100);
 showBalance("500");
 
 const arr2: Array<number | string | boolean> = [1, 'teste', true];
+
+// 11 - validando tipo, avançando em union types
+
+function showUserRole(role: boolean | string) {
+  if(typeof role === 'boolean') return 'Usuário não aprovado!';
+
+  return `A função do usuário é: ${role}`;
+};
+
+showUserRole(false);
+showUserRole('user');
